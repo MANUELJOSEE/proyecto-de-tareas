9c59795fc1f5e2a48d77d1f9e19ac5de03715aeb
MANUELJOSEE <mrodriguezs@fwdcostarica.com>
2023-07-27 15:38:07 -0600

    actualizacion 27/7/23

--- index.js
+++ index.js
@@ -1,35 +1,53 @@
 const txtTareas = document.getElementById("Tareas");
 const btnButton = document.getElementById("Button");
 const Presultado = document.getElementById("resultado");
+const numero = document.getElementById("numero");
 
 const vector = [];
+let contador = 0;
 
 btnButton.addEventListener("click", function (e) {
   e.preventDefault();
 
   const tareas = txtTareas.value;
+  const div = document.createElement("div");
+  div.classList = "tarea";
 
-  // if (tareas) {
-  //   // Presultado.innerText = tareas;
-  //   vector.push(tareas);
-  // }
-
-  // const ul = createElement("ul");
-  console.log(tareas);
   const li = document.createElement("li");
-
   li.textContent = tareas;
 
-  Presultado.appendChild(li);
-});
+  const button = document.createElement("button");
+  button.textContent = "X";
+  button.classList = "btn-delete";
+  //
+  const check = document.createElement("input");
+  check.type = "checkbox";
+  check.className = "checkboxes";
+  //
+  div.appendChild(check);
+  div.appendChild(li);
+  div.appendChild(button);
+  Presultado.appendChild(div);
 
-function addDeleteBtn() {
-  const addDeleteBtn = document.createElement("button");
-
-  deleteBtn.textcontent = "X";
-  deleteBtn.className = "btn";
-
-  deleteBtn.addEventListener("click", (e) => {
-    const item = e.target.pareElement;
+  button.addEventListener("click", function () {
+    if (numero.textContent <= 0) {
+      div.remove();
+    } else {
+      div.remove();
+      contador--;
+      numero.textContent = contador;
+    }
   });
-}
+
+  check.addEventListener("click", function () {
+    if (check.checked) {
+      console.log("sumamos");
+      contador++;
+      numero.textContent = contador;
+    } else {
+      console.log("resta");
+      contador--;
+      numero.textContent = contador;
+    }
+  });
+});
